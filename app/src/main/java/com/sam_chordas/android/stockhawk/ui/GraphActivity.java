c8df6417cbd2672fc0e643c092ba6ee6258b2063
Thanawat Hanthong <peet6015@gmail.com>
2016-05-14 23:32:32 +0700

    can open a new activity from stock

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/GraphActivity.java
@@ -0,0 +1,12 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
