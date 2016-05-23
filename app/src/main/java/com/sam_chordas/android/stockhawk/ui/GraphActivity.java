9614747fe1c4344f3033117994c074de9d0b03f8
Thanawat Hanthong <peet6015@gmail.com>
2016-05-23 15:15:10 +0700

    Draw Chart of symbol

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/GraphActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/GraphActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -22,8 +23,9 @@ import java.util.ArrayList;
 
 public class GraphActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    private static final int CURSOR_LOADER_ID = 0;
-
+    private static final int CURSOR_LOADER_ID = 2;
+    private String mSymbol;
+    private LineChart mChart;
 
 
     @Override
@@ -31,63 +33,22 @@ public class GraphActivity extends AppCompatActivity implements LoaderManager.Lo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_graph);
 
-        LineChart chart = (LineChart) findViewById(R.id.chart1);
-
-        ArrayList<String> xVals = new ArrayList<String>();
-        int count = 20;
-        int range = 100;
-        for (int i = 0; i < count; i++) {
-            xVals.add((i) + "");
-        }
-
-        ArrayList<Entry> yVals = new ArrayList<Entry>();
-
-        for (int i = 0; i < count; i++) {
-
-            float mult = (range + 1);
-            float val = (float) (Math.random() * mult) + 3;// + (float)
-            // ((mult *
-            // 0.1) / 10);
-            yVals.add(new Entry(val, i));
-        }
-
-        LineDataSet set1;
-        set1 = new LineDataSet(yVals, "DataSet 1");
-
-        // set1.setFillAlpha(110);
-        // set1.setFillColor(Color.RED);
-
-        // set the line to be drawn like this "- - - - - -"
-        set1.setColor(Color.BLUE);
-        set1.setCircleColor(Color.BLUE);
-        set1.setLineWidth(1f);
-        set1.setCircleRadius(3f);
-        set1.setDrawCircleHole(false);
-        set1.setValueTextSize(9f);
-        set1.setDrawFilled(true);
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(set1); // add the datasets
-
-        // create a data object with the datasets
-        LineData data = new LineData(xVals, dataSets);
-        chart.setData(data);
+        Bundle extras = getIntent().getExtras();
+        mSymbol= extras.getString(MyStocksActivity.Symbol_key);
 
 
+        mChart = (LineChart) findViewById(R.id.chart1);
         getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
     }
-
-
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String select = "(("+QuoteColumns.ISCURRENT + " = ? ) AND ("+QuoteColumns.SYMBOL + " = ? ))";
-
         return new CursorLoader(this, QuoteProvider.Quotes.CONTENT_URI,
                 new String[]{ QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
                         QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP},
                 select,
-                new String[]{"0","YHOO"},
+                new String[]{"0",mSymbol},
                 null);
     }
 
@@ -99,11 +60,58 @@ public class GraphActivity extends AppCompatActivity implements LoaderManager.Lo
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mChart.clear();
+        mChart.setDescription("A data From "+mSymbol);
+        mChart.setFocusable(true);
+        Legend legend = mChart.getLegend();
+        legend.setTextSize(15f);
+
+        mChart.setData(setChartData(data));
+        mChart.setDescriptionColor(Color.BLUE);
+
+
         Log.d("Cursor",String.valueOf(data.getCount() ));
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    private LineData setChartData(Cursor data)
+    {
+        data.moveToFirst();
+        ArrayList<String> xVals = new ArrayList<String>();
+        int count = data.getCount();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
+        }
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        for (int i = 0; i < count; i++) {
+            float bidData = Float.valueOf(data.getString(2));
+            yVals.add(new Entry(bidData, i));
+            data.moveToNext();
+        }
+
+        LineDataSet set1;
+        set1 = new LineDataSet(yVals, mSymbol);
+
+         //set1.setFillAlpha(110);
+         //set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.setColor(Color.BLUE);
+        set1.setCircleColor(Color.BLUE);
+        set1.setLineWidth(1f);
+        set1.setCircleRadius(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(12f);
+        set1.setDrawFilled(true);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+         return new LineData(xVals, dataSets);
     }
 }
